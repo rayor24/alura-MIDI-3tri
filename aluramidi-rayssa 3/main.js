a5281f77d362aaa9843d334da213e26c26b1cc7a
rayor24 <129771356+rayor24@users.noreply.github.com>
2023-10-09 11:41:22 -0300

    Add files via upload

--- aluramidi-rayssa 3/main.js
+++ aluramidi-rayssa 3/main.js
@@ -1,57 +1,21 @@
+// Pom ----------------------------------------
 function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
-       
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+ 
+const listaDeTeclas =document.querySelectorAll('.tecla');
+
+listaDeTeclas [0].onclick = tocaSomPom;
+
+listaDeTeclas [1].onclick = tocaSomClap;
+listaDeTeclas [2].onclick = tocaSomTim;
+listaDeTeclas [3].onclick = tocaSomPuff;
+listaDeTeclas [4].onclick = tocaSomSplash;
+listaDeTeclas [5].onclick = tocaSomToim;
+listaDeTeclas [6].onclick = tocaSomPsh;
+listaDeTeclas [7].onclick = tocaSomTic;
+listaDeTeclas [8].onclick = tocaSomTom;
 
 
-function tocaSomClap () {
-    document.querySelector('#som_tecla_clap').play();
-}
-       
-document.querySelector('.tecla_clap').onclick = tocaSomClap;
-
-function tocaSomTim () {
-    document.querySelector('#som_tecla_tim').play();
-}
-       
-document.querySelector('.tecla_tim').onclick = tocaSomTim;
 
 
-function tocaSomPuff () {
-    document.querySelector('#som_tecla_puff').play();
-}
-       
-document.querySelector('.tecla_puff').onclick = tocaSomPuff;
-
-function tocaSomSplah () {
-    document.querySelector('#som_tecla_splah').play();
-}
-       
-document.querySelector('.tecla_splah').onclick = tocaSomSplah;
-
-function tocaSomToim () {
-    document.querySelector('#som_tecla_toim').play();
-}
-       
-document.querySelector('.tecla_toim').onclick = tocaSomToim;
-
-function tocaSomPsh () {
-    document.querySelector('#som_tecla_psh').play();
-}
-       
-document.querySelector('.tecla_psh').onclick = tocaSomPsh;
-
-
-function tocaSomTic () {
-    document.querySelector('#som_tecla_tic').play();
-}
-       
-document.querySelector('.tecla_tic').onclick = tocaSomTic;
-
-
-function tocaSomTom () {
-    document.querySelector('#som_tecla_tom').play();
-}
-       
-document.querySelector('.tecla_tom').onclick = tocaSomTom;
