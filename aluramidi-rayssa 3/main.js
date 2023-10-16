1ea929115e292683a803a749ffcfeb9fb7f5ada2
rayor24 <129771356+rayor24@users.noreply.github.com>
2023-10-16 11:44:47 -0300

    Add files via upload

--- aluramidi-rayssa 3/main.js
+++ aluramidi-rayssa 3/main.js
@@ -1,20 +1,23 @@
-// Pom ----------------------------------------
+// Pom-----------------------------------------
 function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
- 
-const listaDeTeclas =document.querySelectorAll('.tecla');
 
-listaDeTeclas [0].onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas [1].onclick = tocaSomClap;
-listaDeTeclas [2].onclick = tocaSomTim;
-listaDeTeclas [3].onclick = tocaSomPuff;
-listaDeTeclas [4].onclick = tocaSomSplash;
-listaDeTeclas [5].onclick = tocaSomToim;
-listaDeTeclas [6].onclick = tocaSomPsh;
-listaDeTeclas [7].onclick = tocaSomTic;
-listaDeTeclas [8].onclick = tocaSomTom;
+let contador = 0;
+
+//enquanto
+while (contador < 9) {
+    
+    listaDeTeclas[0].onclick = tocaSomPom;
+
+    contador = contador + 1;
+
+    console.log(contador);
+
+}
+
 
 
 
