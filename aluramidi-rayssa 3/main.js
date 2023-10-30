6f51bdfe7164f60f4fa95891e7ae5793caa15f4a
rayor24 <129771356+rayor24@users.noreply.github.com>
2023-10-30 11:37:50 -0300

    Add files via upload

--- aluramidi-rayssa 3/main.js
+++ aluramidi-rayssa 3/main.js
@@ -1,16 +1,35 @@
 function tocaSom (SeletorAudio ) {
+    
     const elemento = document.querySelector(seletorAudio);
     
     if (elemento && elemento.localName === 'audio') {
         elemento.play();
-}
+    }
     else {
-
-
-}
+          //alert('Elemento não encontrado');
+          console.log('Elemento ou selector não encontrado!')
+    }
     
 }
 
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+const tecla = listaDeTeclas[contador];
+const instrumento = tecla.classList[1];
+const idAudio = `#som_$(instrumento)`;//template string
+
+tecla.onclick = function () {
+    tocaSom(idAudio);
+}
+
+tecla.onkeydown = function (evento) {
+    if (evento.code === 'Enter' || evento.code === 'Space') {
+
+}
+}
 
 
 
